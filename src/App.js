a3e6555630ef1207152263b2dce53643c053d29d
Faiz-Ahmad-Khan <faizahmadkhan999@gmail.com>
2023-03-26 02:42:40 -0700

    Added Profile

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Login from './components/Login';
 import AddProduct from './components/AddProduct';
 import ProductList from './components/ProductList';
 import UpdateProduct from './components/UpdateComponent';
+import Profile from './components/Profile';
 function App(){
   return(
     <div className="App">
@@ -19,7 +20,7 @@ function App(){
         <Route path="/add" element={<AddProduct />} />
         <Route path="/update/:id" element={<UpdateProduct />} />
         <Route path="/logout" element={<h1>Logout Component</h1>} />
-        <Route path="/profile" element={<h1>Profile Component</h1>} />
+        <Route path="/profile" element={<Profile />} />
         </Route>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
---
+++ src/components/Profile.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Profile = () => {
+  const [user, setUser] = useState({});
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = JSON.parse(localStorage.getItem('user'));
+    if (!auth) {
+      navigate('/login');
+    } else {
+      setUser(auth);
+      getProducts(auth._id);
+    }
+  }, []);
+
+  const getProducts = async (userId) => {
+    let result = await fetch(`http://localhost:5000/products/user/${userId}`, {
+      headers: {
+        authorization: JSON.parse(localStorage.getItem('token')),
+      },
+    });
+    result = await result.json();
+    setProducts(result);
+  };
+
+  return (
+    <div className="profile">
+      <h1>Profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <h2>Products added by you:</h2>
+      {products.length > 0 ? (
+        <ul>
+          {products.map((product) => (
+            <li key={product._id}>{product.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No products added yet.</p>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
