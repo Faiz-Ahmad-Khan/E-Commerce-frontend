fc01745536921092ecbb1ea4ee09179fb9216f8a
Faiz Ahmad Khan <faizahmadkhan999@gmail.com>
2024-01-14 01:16:33 +0530

    Update Product

--- src/components/UpdateComponent.js
+++ src/components/UpdateComponent.js
@@ -44,11 +44,28 @@ const UpdateProduct = () => {
     };
 
     return (
-        <div className="product">
-            {/* ... rest of the component ... */}
-            <button onClick={updateProduct} className="appButton">Update Product</button>
+        <div className='product'>
+            <h1>Update Product</h1>
+            <input type="text" placeholder='Enter product name' className='inputBox'
+                value={name} onChange={(e) => { setName(e.target.value) }}
+            />
+
+            <input type="text" placeholder='Enter product price' className='inputBox'
+                value={price} onChange={(e) => { setPrice(e.target.value) }}
+            />
+
+            <input type="text" placeholder='Enter product category' className='inputBox'
+                value={category} onChange={(e) => { setCategory(e.target.value) }}
+            />
+
+            <input type="text" placeholder='Enter product company' className='inputBox'
+                value={company} onChange={(e) => { setCompnay(e.target.value) }}
+            />
+
+
+            <button onClick={updateProduct} className='appButton'>Update Product</button>
         </div>
-    );
-};
+    )
+}
 
 export default UpdateProduct;
